94398b414a1317c4a47b4890473f78388cb42e6d
ReDxDaGer <yashmehrotra45@gmail.com>
2023-12-13 19:29:44 +0530

    Updated

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 Shery.mouseFollower();
 Shery.makeMagnet('.magnet');
-function headingAnimation(){
-    gsap.from("#page1 h1",{
-        y:100,
-        opacity:0,
-        delay:1,
-        duration:0.5,    
-        stagger:0.3
-  })}
+
 function bttnAnimation(){
     gsap.from(".btn a",{
         y:200,
@@ -25,16 +18,32 @@ function navAnimation(){
     })
 }
 function cardAnimation(){
-    gsap.from('.card',{
+    gsap.from('.team__member-image',{
         scale:0,
         ease:"elastic.out(0.6,0.5)",
         opacity:0,
-        delay:2,
+        delay:1.5,
+        duration:1.3,
+        stagger:0.1
+    })
+    gsap.from('.team__member-info',{
+        scale:0,
+        ease:"elastic.out(0.6,0.5)",
+        opacity:0,
+        delay:1.5,
         duration:1.3,
         stagger:0.1
     })
 }
+function h1Animation(){
+    gsap.from("#page1 h1",{
+        y:0,
+        opacity:1,
+        delay:1,
+        duration:0.5,    
+        stagger:0.3
+    })}
 cardAnimation();
 navAnimation();
 bttnAnimation();
-headingAnimation();
+h1Animation();
